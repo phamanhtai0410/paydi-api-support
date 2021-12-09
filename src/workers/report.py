de62082a0b2e4bda036663052ab8d15dc73a837b
PAT <phamanhtai0410@gmail.com>
2021-12-09 16:30:43 +0700

    add env vars

--- src/workers/report.py
+++ src/workers/report.py
@@ -144,9 +144,9 @@ class ReportWorker(object):
                             password=DefaultConfig.ODOO_PASSWORD)
 
         client.create(model_name="helpdesk.ticket", data_dict={
-            "partner_name": "CS Support",
-            "partner_id": 18,
-            "partner_email": "cs.support@paydi.vn",
+            "partner_name": DefaultConfig.ODOO_TICKET_PARTNER_NAME,
+            "partner_id": int(DefaultConfig.ODOO_TICKET_PARTNER_ID),
+            "partner_email": DefaultConfig.ODDO_TICKET_PARTNER_EMAIL,
             "name": '# Customer Report',
             "attachment_ids": False,
             "channel_id": 2,
